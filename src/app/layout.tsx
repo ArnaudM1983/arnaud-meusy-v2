d67e891197c3080ae6db446b602c7dc7c565f63c
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-03-27 17:51:50 +0100

    Initial commit / Mise en place config + ajout du Hero
    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
   subsets: ["latin"],
 });
 
@@ -23,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr" className="bg-slate-900 text-slate-100">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={urbanist.variable}
       >
         {children}
       </body>
