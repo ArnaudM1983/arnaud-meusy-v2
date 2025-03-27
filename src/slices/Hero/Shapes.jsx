0e0667fbb841a6b27dcf54cfd0b6389b8bc9669f
ArnaudM1983 <arnaudmeusy@hotmail.com>
2025-03-27 20:57:59 +0100

    Ajout du modèle 3d sur le hero

---
+++ src/slices/Hero/Shapes.jsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import { Suspense, useRef, useState, useEffect } from "react";
+import { gsap } from "gsap";
+import { useGLTF } from "@react-three/drei"; // Importation de useGLTF pour charger le modèle
+
+// Composant principal pour afficher le modèle
+export default function Shapes() {
+  return (
+    <div className="relative w-full h-full">
+      <Canvas
+        className="z-0"
+        shadows
+        gl={{ antialias: false }}
+        dpr={[1, 1.5]}
+        camera={{ position: [0, 5, 11], fov: 50, near: 1, far: 40 }}
+      >
+        <Suspense fallback={null}>
+          <Laptop /> 
+          
+          {/* Lumière directionnelle avec position modifiée */}
+          <directionalLight
+            position={[5, 10, 5]} // Position de la lumière directionnelle
+            intensity={1} // Intensité de la lumière
+            castShadow
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+          />
+          
+          <ContactShadows
+            position={[0, -3.5, 0]}
+            opacity={0.65}
+            scale={40}
+            blur={1}
+            far={9}
+          />
+          
+          <Environment preset="city" />
+          <OrbitControls enableZoom={false}/> {/* Ajouter OrbitControls pour permettre l'interaction avec la caméra */}
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
+
+// Composant pour afficher le modèle 3D du laptop
+function Laptop() {
+  // Charger le modèle 3D laptop.glb à partir du dossier public
+  const { scene } = useGLTF("/laptop.glb");
+  const meshRef = useRef();
+  const [visible, setVisible] = useState(false);
+
+  // Animation d'apparition
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      setVisible(true);
+      gsap.from(meshRef.current.scale, {
+        x: 0,
+        y: 0,
+        z: 0,
+        duration: gsap.utils.random(0.8, 1.2),
+        ease: "elastic.out(1,0.3)",
+        delay: gsap.utils.random(0, 0.5),
+      });
+
+      // Animation de rotation continue qui s'inverse à chaque cycle
+      gsap.to(meshRef.current.rotation, {
+        y: "-=2 * Math.PI", // Rotation continue sur l'axe Y
+        duration: 10, // Durée d'une rotation complète
+        repeat: -1, // Répéter indéfiniment
+        yoyo: true, // Faire revenir l'animation dans l'autre sens après chaque cycle
+        ease: "linear", // Rotation constante
+      });
+    });
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <group ref={meshRef} position={[0, 0, 0]}>
+      {/* Ici, nous utilisons la scène du modèle GLB */}
+      <primitive object={scene} visible={visible} />
+    </group>
+  );
+}
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -3,9 +3,9 @@
 import { FC } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
-
+import Shapes from "./Shapes";
 /**
  * Props for `Hero`.
  */
@@ -32,6 +32,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
           ease: "elastic.out(1,0.3)",
           duration: 1,
           transformOrigin: "left top",
+          delay: 0.5,
           stagger: {
             each: 0.1,
             from: "random",
@@ -69,9 +70,10 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      ref={component}
+      ref={component} className="px-4 py-10 md:px-6 md:py-14 lg:py-16 mx-auto w-full max-w-7xl"
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
             aria-label={slice.primary.prenom + " " + slice.primary.nom}>
@@ -79,7 +81,7 @@ const Hero: FC<HeroProps> = ({ slice }) => {
             <span className="-mt-[0,2em] block text-slate-500">{renderLetters(slice.primary.nom, "first")}</span>
           </h1>
           <span className="job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl 
-          font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-5xl">{slice.primary.accroche}</span>
+          font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-5xl">{slice.primary.accroche}</span>
         </div>
       </div>
     </section>
